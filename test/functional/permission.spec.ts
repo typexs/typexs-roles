a171c3c9f6230943f548053a326125de780b4f53
Robert Kania <cezaryrk@yahoo.de>
2020-02-02 15:32:09 +0100

    test: add basic permission check

---
+++ test/functional/permission.spec.ts
@@ -0,0 +1,25 @@
+import {expect} from 'chai';
+import {Bootstrap, Container} from '@typexs/base';
+import {suite, test} from 'mocha-typescript';
+import {BasicPermission} from '../../packages/typexs-roles-api/src';
+
+
+@suite('functional/permission')
+class PermissionSpec {
+
+  @test()
+  'basic'() {
+    let p = new BasicPermission('test');
+    expect(p.permission).to.be.eq('test');
+    expect(p.type).to.be.eq('single');
+
+    p = new BasicPermission('test * object');
+    expect(p.permission).to.be.eq('test * object');
+    expect(p.type).to.be.eq('pattern');
+
+    p = new BasicPermission('test object');
+    expect(p.permission).to.be.eq('test object');
+    expect(p.type).to.be.eq('single');
+  }
+
+}
