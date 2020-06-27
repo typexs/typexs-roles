0b1ff99bddb95adc01807924ec0a27da241a436d
Robert Kania <cezaryrk@yahoo.de>
2020-06-28 00:21:11 +0200

    fix: activator

--- src/Activator.ts
+++ src/Activator.ts
@@ -1,4 +1,4 @@
-import {ClassesLoader, Container, IActivator, Injector} from '@typexs/base';
+import {ClassesLoader,  IActivator, Injector} from '@typexs/base';
 import {PermissionsRegistry} from './libs/PermissionsRegistry';
 import {PermissionsRegistryLoader} from './libs/PermissionsRegistryLoader';
 import {BasicPermission, IPermissionDef, IPermissions} from '@typexs/roles-api';
@@ -8,10 +8,10 @@ export class Activator implements IActivator, IPermissions {
 
   startup(): void {
     const r = Injector.create(PermissionsRegistry);
-    Container.set(PermissionsRegistry.NAME, r);
+    Injector.set(PermissionsRegistry.NAME, r);
 
     const l = Injector.create(PermissionsRegistryLoader);
-    Container.set(PermissionsRegistryLoader.NAME, l);
+    Injector.set(PermissionsRegistryLoader.NAME, l);
   }
 
 
